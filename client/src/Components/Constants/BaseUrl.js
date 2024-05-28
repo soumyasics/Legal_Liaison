daf6635fcfaec90869003b786a5bef664fcf406a
RadhulSics <radhul.sics@gmail.com>
2024-05-28 16:55:30 +0530

    sprint1 bug fixed

--- client/src/Components/Constants/BaseUrl.js
+++ client/src/Components/Constants/BaseUrl.js
@@ -4,12 +4,12 @@ const axiosInstance = axios.create({
 
   //server api
   
-  // baseURL: 'http://hybrid.srishticampus.in:4043/legal_liaison_api/', 
+  baseURL: 'http://hybrid.srishticampus.in:4043/legal_liaison_api/', 
 
 
 //local api
 
-baseURL: 'http://localhost:4043/legal_liaison_api/', 
+// baseURL: 'http://localhost:4043/legal_liaison_api/', 
 
   headers: {
     'Content-Type': 'application/json',
